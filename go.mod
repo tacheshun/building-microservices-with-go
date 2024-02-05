0698824c858dddccb75d57ab1ec5e331f83b9dcd
Marius Costache <marius.costache@onapsis.com>
2024-02-05 22:55:04 +0200

    wip until rest api

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/tacheshun/building-microservices-with-go
 
 go 1.21
+
+require github.com/nicholasjackson/env v0.6.1
--- main.go
+++ main.go
@@ -1,26 +1,48 @@
 package main
 
 import (
-	"fmt"
-	"io"
+	"context"
+	"github.com/nicholasjackson/env"
+	"github.com/tacheshun/building-microservices-with-go/handlers"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
+var bindAddress = env.String("BIND_ADDRESS", false, ":9090", "Bind address for the server")
+var bindPort = env.Int("BIND_PORT", true, 0, "bind port for server, i.e. 9090")
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("hello world")
-		d, err := io.ReadAll(r.Body)
+	env.Parse()
+	
+	l := log.New(os.Stdout, "hello", log.LstdFlags)
+	ph := handlers.NewProducts(l)
+	sm := http.NewServeMux()
+	sm.Handle("/", ph)
+
+	s := &http.Server{
+		Addr:         ":9090",
+		Handler:      sm,
+		WriteTimeout: 1 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		IdleTimeout:  30 * time.Second,
+	}
+	go func() {
+		err := s.ListenAndServe()
 		if err != nil {
-			http.Error(w, "Ooops", http.StatusBadRequest)
-			return
+			l.Fatal(err)
 		}
-		fmt.Fprintf(w, "Hello %s", d)
-	})
+	}()
 
-	http.HandleFunc("/goodbye", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Goodbye world")
-	})
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+	sig := <-sigChan
+	l.Println("Received terminate, graceful shutdown", sig)
 
-	log.Fatal(http.ListenAndServe(":9090", nil))
+	tc, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	s.Shutdown(tc)
 }
